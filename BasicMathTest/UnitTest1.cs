2be8c93a345ed9656dfbe5140796ed669c31b746
Bethany Hanrahan <bethanyhanrahan123@gmail.com>
2020-06-12 18:29:15 +0100

    Add project files.

---
+++ BasicMathTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestPractice;
+
+namespace BasicMathTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //test the happy path for Add Method in Basic Math class
+        [TestMethod]
+       public void TestAddMethod_Correct()
+        {
+            BasicMaths bm = new BasicMaths(); //instance of the basic maths class - contains units(methods) to test
+            double result = bm.AddNums(15, 10);
+            Assert.AreEqual(result, 25); // checks if the test data matches what the answer should be
+        }
+
+        [TestMethod]
+        public void TestSubMethod_Correct()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.SubNums(15, 10);
+            Assert.AreEqual(result, 5);
+        }
+
+        [TestMethod]
+        public void TestMultMethod_Correct()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.MultNum(10, 5);
+            Assert.AreEqual(result, 50);
+        }
+
+        [TestMethod]
+        public void TestDivMethod_Correct()
+        {
+            BasicMaths bm = new BasicMaths();
+            double result = bm.DivNum(10, 2);
+            Assert.AreEqual(result, 5);
+        }
+    }
+}
---
+++ UnitTestPractice/BasicMaths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestPractice
+{
+    public class BasicMaths
+    {
+        public double AddNums(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double SubNums(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double MultNum(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double DivNum(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
